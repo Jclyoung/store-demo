ae24c17fca66c645963669ef404d8b06126f0499
Jennifer Young <jlynnyoung1@gmail.com>
2023-11-14 17:51:14 -0600

    update bd.json, readme, app.jsx - remove index.css, manifest.json,

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ npm start
 Install dependencies and starts the mock api
 
 ## Project Overview
+The purpose of this project is to give a broad overview of obtained knowledge and skills. This is not meant to cover all skills and knowledge.
+
+Created with [Create React App](https://github.com/facebook/create-react-app).
 
 1. Added a mock API 
 2. Configured `npm start`
@@ -20,10 +23,13 @@ Install dependencies and starts the mock api
     - [cross-env](https://www.npmjs.com/search?q=cross-env for setting environment variables
 4. Added styles to App.css
 5. Added `/public/images`.
-6. 
+6. added 'error handling.
 7. Added data fetching in `/src/services`.
 8. Added db.json to root as json-server's mock database
 9. Overwrote App.css with custom styles
 10. Simplified index.js (removed service worker)
 11. Deleted from src: index.css, logo.svg, serviceWorker.js, App.test.js
 12. Deleted from public: logo files, manifest.json, robots.txt
+13. Change product.skus to product.data to be able better define the array of objects
+14. Add useFetch to collect data from mockdb
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Footer from "./layout/Footer";
-import Header from "./layout/Header";
+import Header from "./Header";
+import Footer from "./Footer";
+import Spinner from "./Spinner";
+import useFetch from "./services/useFetch";
 
 export default function App() {
+  const [size, setSize] = useState("");
+  const { data: products, loading, error } = useFetch(
+    "products?category=shirts"
+  );
+
+  const filteredProducts = size
+    ? products.filter((p) => p.items.find((s) => s.size === parseInt(size)))
+    : products;
+
+  if (error) throw error;
+  if (loading) return <Spinner />;
+  
+  function renderProduct(p) {
+    return (
+      <section key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </section>
+    );
+  }
+
   return (
     <>
       <div className="content">
@@ -11,13 +37,19 @@ export default function App() {
         <main>
           <section id="filters">
             <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size">
+            <select
+              id="size"
+              value={size}
+              onChange={(e) => setSize(e.target.value)}
+            >
               <option value="">All sizes</option>
               <option value="7">7</option>
               <option value="8">8</option>
               <option value="9">9</option>
             </select>
+            {size && <h2>Found {filteredProducts.length} items</h2>}
           </section>
+          <section id="products">{filteredProducts.map(renderProduct)}</section>
         </main>
       </div>
       <Footer />
